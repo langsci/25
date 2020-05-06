5c84dd6e27a629dc3daecbdf864276fe0dfc8eaa
Sašo Živanović <saso.zivanovic@guest.arnes.si>
2020-05-06 12:13:04 +0200

    Reformat the .for file using "for-reformat.sh".

---
+++ for-reformat.sh
@@ -0,0 +1,1 @@
+sed -i 's/\\forest@external/\\forestLoadExternal/g; s/\\forest@includeexternal@check/\\forestIncludeExternalCheck/g; s/\\forest@includeexternal/\\forestIncludeExternal/g; s/\\forest@includeexternal@box/\\forestIncludeExternalBox/g; ' "$1"
